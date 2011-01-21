7dfa2540f55814e542d36223367b28cda75da62f
Kwan Lim <ppc.coder@gmail.com>
2011-01-21 12:41:08 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Promo::Application.config.secret_token = '6e53a4c9a8ed1c7210ce51156e7ee5f91b00fb350408cefff44da2d4e6be445bd2219f12fddf600ae49993a6c09ff293bf5fa335516e7f6a97be33a4e08c8ecd'
